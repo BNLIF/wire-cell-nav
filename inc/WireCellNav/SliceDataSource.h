9bd7cada89b2510a6542b21abdd3ba87244f0739
Brett Viren <brett.viren@gmail.com>
2015-04-04 16:58:06 -0400

    Respond to data changes.

--- inc/WireCellNav/SliceDataSource.h
+++ inc/WireCellNav/SliceDataSource.h
@@ -12,19 +12,6 @@ namespace WireCell {
        SliceDataSource - deliver slices of frames from a FrameDataSource
      */
     class SliceDataSource {
-	WireCell::FrameDataSource& fds;
-
-	// needed to resolve electronics channel->wire IDs
-	const WireCell::GeomDataSource& gds; 
-	WireCell::Slice slice;	// cache the current slice
-	int frame_index;	// last frame we loaded
-	int slice_index;	// current slice, for caching
-	mutable int slices_begin; // tbin index of earliest bin of all traces
-	mutable int slices_end;	  // tbin index of one past the latest bin of all traces
-
-	void update_slices_bounds() const;
-	void clear();
-
     public:
 
 	SliceDataSource(WireCell::FrameDataSource& fds, const WireCell::GeomDataSource& gds);
@@ -42,6 +29,22 @@ namespace WireCell {
 	/// Get the current slice
 	WireCell::Slice&  get();
 	const WireCell::Slice&  get() const;
+
+    private:
+
+	WireCell::FrameDataSource& _fds;
+
+	// needed to resolve electronics channel->wire IDs
+	const WireCell::GeomDataSource& _gds; 
+	WireCell::Slice _slice;	// cache the current slice
+	int _frame_index;	// last frame we loaded
+	int _slice_index;	// current slice, for caching
+	mutable int _slices_begin; // tbin index of earliest bin of all traces
+	mutable int _slices_end; // tbin index of one past the latest bin of all traces
+
+
+	void update_slices_bounds() const;
+	void clear();
 
     };
 
--- src/SliceDataSource.cxx
+++ src/SliceDataSource.cxx
@@ -3,12 +3,12 @@
 using namespace WireCell;
 
 SliceDataSource::SliceDataSource(FrameDataSource& fds, const GeomDataSource& gds)
-    : fds(fds)
-    , gds(gds)
-    , frame_index(-1)
-    , slice_index(-1)
-    , slices_begin(-1)
-    , slices_end(-1)
+    : _fds(fds)
+    , _gds(gds)
+    , _frame_index(-1)
+    , _slice_index(-1)
+    , _slices_begin(-1)
+    , _slices_end(-1)
 {
     update_slices_bounds();
 }
@@ -19,20 +19,20 @@ SliceDataSource::~SliceDataSource()
 
 void SliceDataSource::clear()
 {
-    slice_index = slices_begin = slices_end = -1;
-    slice.clear();
+    _slice_index = _slices_begin = _slices_end = -1;
+    _slice.clear();
 }
 
 // Update the bounds on the slice indices based on the current frame.
 void SliceDataSource::update_slices_bounds() const
 {
-    const Frame& frame = fds.get();
+    const Frame& frame = _fds.get();
 
     if (frame.index < 0) {	// no frames
 	return;
     }
 
-    if (frame.index == frame_index) { // no change, no-op
+    if (frame.index == _frame_index) { // no change, no-op
 	return;
     }
 
@@ -43,14 +43,14 @@ void SliceDataSource::update_slices_bounds() const
 	const Trace& trace = frame.traces[ind];
 
 	if (!ind) {		// first time
-	    slices_begin = trace.tbin;
-	    slices_end   = trace.charge.size() + slices_begin;
+	    _slices_begin = trace.tbin;
+	    _slices_end   = trace.charge.size() + _slices_begin;
 	    continue;
 	}
 	int tbin = trace.tbin;
 	int nbins = trace.charge.size();
-	slices_begin = std::min(slices_begin, tbin);
-	slices_end   = std::max(slices_end,   tbin + nbins);
+	_slices_begin = std::min(_slices_begin, tbin);
+	_slices_end   = std::max(_slices_end,   tbin + nbins);
     }
     return;
 }
@@ -58,10 +58,10 @@ void SliceDataSource::update_slices_bounds() const
 int SliceDataSource::size() const
 {
     this->update_slices_bounds();
-    if (slices_begin < 0 || slices_end < 0) {
+    if (_slices_begin < 0 || _slices_end < 0) {
 	return 0;
     }
-    return slices_end - slices_begin;
+    return _slices_end - _slices_begin;
 }
 
 int SliceDataSource::jump(int index)
@@ -73,60 +73,61 @@ int SliceDataSource::jump(int index)
 
     this->update_slices_bounds();
 
-    if (index >= slices_end) {	// overflow
+    if (index >= _slices_end) {	// overflow
 	this->clear();
 	return -1;
     }
-    if (index == slice_index) {
+    if (index == _slice_index) {
 	return index;		// already loaded, no-op
     }
 
     // new slice
 
-    slice.clear();
-    slice_index = index;	
-    slice.tbin = slice_index + slices_begin; 
+    _slice.clear();
+    _slice_index = index;	
+    int slice_tbin = _slice_index + _slices_begin; 
+    Wire::Group slice_group;
 	
-    const Frame& frame = fds.get();
+    const Frame& frame = _fds.get();
     size_t ntraces = frame.traces.size();
     for (size_t ind=0; ind<ntraces; ++ind) {
 	const Trace& trace = frame.traces[ind];
 	int tbin = trace.tbin;
 	int nbins = trace.charge.size();
 
-	if (slice.tbin < tbin) {
+	if (slice_tbin < tbin) {
 	    continue;
 	}
-	if (slice.tbin >= tbin+nbins) {
+	if (slice_tbin >= tbin+nbins) {
 	    continue;
 	}
 
 	// Save association of a wire (segment) ID and charge.  Note,
 	// the inherent degeneracy in detectors with wrapped wires is
 	// preserved here when the WireSelection is larger than 1 wire.
-	int q = trace.charge[slice.tbin];
-	const GeomWireSelection& ws = gds.by_channel(trace.chid);
+	int q = trace.charge[slice_tbin];
+	const GeomWireSelection& ws = _gds.by_channel(trace.chid);
 	size_t ws_size = ws.size();
 	for (size_t iwid=0; iwid < ws_size; ++iwid) {
-	    int wid = ws[iwid]->ident();
-	    slice.charge.push_back(WireCharge(wid, q));
+	    slice_group.push_back(Wire::Charge(ws[iwid], q));
 	}
     }
+    _slice.reset(slice_tbin, slice_group);
     return index;
 }
 
 int SliceDataSource::SliceDataSource::next()
 {
-    this->jump(slice_index+1);
+    this->jump(_slice_index+1);
 }
 
 Slice& SliceDataSource::SliceDataSource::get()
 {
-    return slice;
+    return _slice;
 }
 
 const Slice& SliceDataSource::SliceDataSource::get() const
 {
-    return slice;
+    return _slice;
 }
 
