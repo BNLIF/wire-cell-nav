efde5fd28c1abc92edf7427eb92a8abea66f0c93
Brett Viren <bv@bnl.gov>
2015-04-13 17:03:06 -0400

    Add a rebinning FDS.  Not yet tested.

---
+++ inc/WireCellNav/RebinningFDS.h
@@ -0,0 +1,22 @@
+#ifndef WIRECELLNAV_REBINNINGFDS
+#define  WIRECELLNAV_REBINNINGFDS
+
+#include "WireCellNav/FrameDataSource.h"
+
+namespace WireCell {
+
+    class RebinningFDS : public FrameDataSource
+    {
+	FrameDataSource& fds;
+	int nbins_to_group;
+    public:
+	RebinningFDS(FrameDataSource& fds, int nbins_to_group);
+	virtual ~RebinningFDS();
+
+	/// Explicitly set the "frame" (event) to process.  Frame number returned or -1 on error.
+	virtual int jump(int frame_number);
+
+    };
+}
+
+#endif
---
+++ src/RebinningFDS.cxx
@@ -0,0 +1,46 @@
+
+#include "WireCellNav/RebinningFDS.h"
+
+using namespace WireCell;
+
+RebinningFDS::RebinningFDS(FrameDataSource& fds, int nbins_to_group)
+    : fds(fds)
+    , nbins_to_group(nbins_to_group)
+{
+}
+RebinningFDS::~RebinningFDS()
+{
+}
+
+int RebinningFDS::jump(int frame_number)
+{
+    int got = fds.jump(frame_number);
+    if (got < 0) {
+	return got;
+    }
+    if (frame.index == got) {
+	return got;		// same frame
+    }
+
+    frame.clear();		// win or lose, we start anew
+
+    const WireCell::Frame& other_frame = fds.get();
+
+    for (std::size_t itrace = 0; itrace < other_frame.traces.size(); ++itrace) {
+	const WireCell::Trace& other_trace = other_frame.traces[itrace];
+	WireCell::Trace trace;
+	trace.chid = other_trace.chid;
+
+	// rebin
+	std::size_t nother_bins = other_trace.charge.size();
+	trace.tbin = other_trace.tbin / nbins_to_group;
+	trace.charge.resize(nother_bins / nbins_to_group + 1, 0);
+	for (std::size_t ibin=0; ibin<nother_bins; ++ibin) {
+	    trace.charge[ibin/nbins_to_group] += other_trace.charge[ibin];
+	}
+	frame.traces.push_back(trace);
+    }
+    frame.index = other_frame.index;
+    return frame.index;
+}
+
