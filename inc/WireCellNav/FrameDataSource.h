5854df5e96270c69d74841f46674410bcf66a794
Brett Viren <bv@bnl.gov>
2015-04-13 17:02:47 -0400

    Reindent.

--- inc/WireCellNav/FrameDataSource.h
+++ inc/WireCellNav/FrameDataSource.h
@@ -10,29 +10,29 @@ namespace WireCell {
 
    This class is mostly abstract.  Subclass implementations should 
  */
-class FrameDataSource {
-protected:
+    class FrameDataSource {
+    protected:
 
-    WireCell::Frame frame;
+	WireCell::Frame frame;
     
-public:
-    FrameDataSource();
-    virtual ~FrameDataSource();
-
-    /// Return the number of frames this data source knows about.  Return -1 if unlimited.
-    virtual int size() const = 0;
-
-    /// Explicitly set the "frame" (event) to process.  Frame number returned or -1 on error.
-    virtual int jump(int frame_number) = 0;
-
-    /// Advance to next frame, return frame number or -1 on error, -2 on end of frames
-    virtual int next();
-
-    /// Access currently loaded frame.
-    virtual WireCell::Frame& get();
-    virtual const WireCell::Frame& get() const;
-
-};
+    public:
+	FrameDataSource();
+	virtual ~FrameDataSource();
+	
+	/// Return the number of frames this data source knows about.  Return -1 if unlimited.
+	virtual int size() const = 0;
+	
+	/// Explicitly set the "frame" (event) to process.  Frame number returned or -1 on error.
+	virtual int jump(int frame_number) = 0;
+	
+	/// Advance to next frame, return frame number or -1 on error, -2 on end of frames
+	virtual int next();
+	
+	/// Access currently loaded frame.
+	virtual WireCell::Frame& get();
+	virtual const WireCell::Frame& get() const;
+	
+    };
 
 }
 
