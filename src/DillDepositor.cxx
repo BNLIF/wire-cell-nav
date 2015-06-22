05c27d147829203e51940e589b09ef222b163e2e
Brett Viren <bv@bnl.gov>
2015-06-22 09:32:29 -0400

    Quiet some cerr's and fix step calc.

--- src/DillDepositor.cxx
+++ src/DillDepositor.cxx
@@ -27,17 +27,22 @@ const PointValueVector& DillDepositor::depositions(int frame_number) const
     std::uniform_real_distribution<float> zrand(zmm.first, zmm.second);
     std::uniform_real_distribution<float> lrand(lmm.first, lmm.second);
 
+    
+    double step_size = 1.0/hit_density;
+    //cerr << "Dill frame: " << frame_number << " step_size: " << step_size << endl;
+
     hits.clear();
     for (int ind=0; ind < dills_per_frame; ++ind) {
 	float length = lrand(gen);
-
+	//cerr << "Dill step length: " << length/units::mm << " mm" << endl;
+	
 	Point p1(xrand(gen), yrand(gen), zrand(gen));
 	hits.push_back(PointValue(p1, unit_charge));
 
 	Point p2(xrand(gen), yrand(gen), zrand(gen));
 	Point dir = Point(p2-p1).norm();
 
-	for (float step = hit_density; step < length; step += hit_density) {
+	for (float step = step_size; step < length; step += step_size) {
 	    Point p = p1 + step * dir;
 	    PointValue pv(p, unit_charge);
 	    //cerr << "hit: " << ind << " q=" << pv.second << " p=" << p << endl;
