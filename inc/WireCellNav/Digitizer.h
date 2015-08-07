3de65cf78eda58539fe66690915d7e97b1a68c29
Brett Viren <bv@bnl.gov>
2015-08-07 17:09:00 -0400

    Initial, trivial digitizer.

--- inc/WireCellNav/Digitizer.h
+++ inc/WireCellNav/Digitizer.h
@@ -1,13 +1,35 @@
 #ifndef WIRECELL_DIGITIZER
 #define WIRECELL_DIGITIZER
 
-#include "WireCellIface/IWire.h"
+#include "WireCellIface/IWireSummary.h"
+#include "WireCellIface/IDepo.h"
+#include "WireCellIface/IFrame.h"
 #include "WireCellUtil/Units.h"
+
 
 namespace WireCell {
 
-    class Digitizer, public IWireSink {
-	typedef boost::signals2::signal<IDepo::const_ptr ()> DepoFeed;
+    /** A simple digitizer of charge drifting past wire planes.
+     *
+     * Each call produces an IFrame which spans a given number of
+     * ticks each of a given size.
+     *
+     * This digitizer is simple in that it:
+     *
+     * - the planes are taken to be coplanar
+     * - the depositions are assumed to be delivered at the common plane 
+     * - the drifting charge is directly turned into signal (no response function, no deconvolution)
+     * - only the closest wire is "hit" (even for induction planes)
+     */
+    class Digitizer : public IWireSummaryClient {
+	typedef boost::signals2::signal<IDepo::pointer ()> DepoFeed;
+	DepoFeed m_feed;
+	const int m_maxticks;
+	const double m_tick;
+	double m_time;
+	IDepo::pointer m_depo;
+	int m_frame_count;
+
     public:
 	/// A slot which feeds the drifter depositions.  Each time it
 	/// is called it should return the next available deposition
@@ -15,19 +37,20 @@ namespace WireCell {
 	typedef DepoFeed::slot_type DepoFeeder;
 
 	/// Make a simple digitizer for the given wire plane and
-	/// digitization tick.
-	Digitizer(double tick = 0.5*units::microsecond);
+	/// digitization tick.  The traces in the resulting frames
+	/// will not span more than maxticks.
+	Digitizer(int maxticks = 1000,
+		  double tick = 0.5*units::microsecond,
+		  double start_time = 0*units::second);
 	// fixme: configurable
 
-	/// Set the wires to use (IWireSink).  
-	virtual void sink(wire_range wires);
-
-	/// Connect a feed of depositions at the wire plane
+	/// Connect a feed of depositions at the wire plane.  This
+	/// digitizer assumes depositions are provided already drifted
+	/// to the wire plane.
 	void connect(const DepoFeeder& feed);
 
-
 	/// Produce a frame.
-	IFrame::const_ptr operator()();
+	IFrame::pointer operator()();
 
     };
 
---
+++ src/Digitizer.cxx
@@ -0,0 +1,136 @@
+#include "WireCellNav/Digitizer.h"
+
+#include <map>
+
+using namespace WireCell;
+
+Digitizer::Digitizer(int maxticks, double tick, double start_time)
+    : m_maxticks(maxticks)
+    , m_tick(tick)
+    , m_time(start_time)
+    , m_depo(0)
+    , m_frame_count(0)
+{
+    
+}
+
+
+class DigitizedTrace : public ITrace {
+    int m_chid;
+    ChargeSequence m_charge;
+public:
+    DigitizedTrace(int chid, int nbins)
+	: m_chid(chid)
+	, m_charge(nbins,0)
+    {
+    }
+
+    void operator()(int bin, float charge) {
+	m_charge[bin] += charge;
+    }
+
+    virtual int channel() const { return m_chid; }
+
+    virtual int tbin() const { return 0; }
+
+    virtual const ChargeSequence& charge() const { return m_charge; };
+};
+
+
+
+class SimpleFrame : public IFrame {
+    int m_ident;
+    double m_time;
+    std::vector<ITrace::pointer> m_traces;
+public:
+    SimpleFrame(int ident, double time, const std::vector<DigitizedTrace*>& traces)
+	: m_ident(ident), m_time(time) {
+	for (auto dt : traces) {
+	    m_traces.push_back(ITrace::pointer(dt));
+	}
+    }
+    virtual int ident() const { return m_ident; }
+    virtual double time() const { return m_time; }
+    
+    virtual iterator begin() { return adapt(m_traces.begin()); }
+    virtual iterator end() { return adapt(m_traces.end()); }
+};
+
+class TraceBuilder {
+
+    std::vector<DigitizedTrace*> traces;
+    typedef std::map<int, int> TraceIndexMap; // channel->index into traces
+    TraceIndexMap tim;
+
+    IWireSummary::pointer ws;
+    int nbins;
+    int nframes;
+public:
+    TraceBuilder(IWireSummary::pointer ws, int nbins)
+	: ws(ws), nbins(nbins), nframes(0) {}
+    ~TraceBuilder() {
+	for (auto t : traces) {
+	    delete t;
+	}
+    }
+
+    void add_depo(const Point& pos, int tbin, double charge) {
+	for (int iplane=0; iplane<3; ++iplane) {
+	    WirePlaneType_t plane = static_cast<WirePlaneType_t>(iplane);
+	    IWire::pointer wire = ws->closest(pos, plane);
+	    if (!wire) { continue; }
+
+	    int chid = wire->channel();
+
+	    TraceIndexMap::iterator it = tim.find(chid);
+	    int trace_index = traces.size(); // if new
+
+	    DigitizedTrace* trace = 0;
+	    if (it == tim.end()) {
+		trace = new DigitizedTrace(chid, nbins);
+	    }
+	    else {
+		trace = traces[it->second];
+	    }
+
+	    (*trace)(tbin, charge);
+	    
+	}
+    }
+    IFrame* make_frame(int ident, double tmin) {
+	IFrame* ret = new SimpleFrame(ident, tmin, traces);
+	traces.clear();	
+    }
+};
+
+
+IFrame::pointer Digitizer::operator()()
+{
+    // prime
+    if (!m_depo) { m_depo = *m_feed(); }
+    if (!m_depo) { return 0; }	// empty
+
+    const double tmin = m_time;
+    const double tmax = m_time + m_maxticks*m_tick;
+    m_time += tmax;		// next start time
+
+    TraceBuilder tm(m_wiresummary, m_maxticks);
+    while (true) {
+	if (!m_depo || m_depo->time() > tmax) {
+	    IFrame* ret = tm.make_frame(m_frame_count, tmin);
+	    ++m_frame_count;
+	    return IFrame::pointer(ret);
+	}
+	
+	int tbin = (m_depo->time() - tmin)/m_tick;
+	tm.add_depo(m_depo->pos(), tbin, m_depo->charge());
+	m_depo = *m_feed();	// set up for next
+    }
+}
+
+
+
+void Digitizer::connect(const DepoFeeder& feed)
+{
+    m_feed.connect(feed);
+}
