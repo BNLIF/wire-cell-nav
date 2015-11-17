c43ac0937a54bac933af866b80d21bb78b803285
Xin Qian <last.qx@gmail.com>
2015-11-16 20:55:16 -0500

    recover application

--- src/DetGenerativeFDS.cxx
+++ src/DetGenerativeFDS.cxx
@@ -80,23 +80,23 @@ int DetGenerativeFDS::jump(int frame_number)
     const WrappedGDS *apa_gds = 0;
 
     //hack for now
-    nhits = 1;
+    //nhits = 1;
 
     for (size_t ind=0; ind<nhits; ++ind) {
-	// const Point& pt = hits[ind].first;
-	// //const Vector vpt(pt.x, pt.y, pt.z);
-	// float charge = hits[ind].second;
-
-      const Point pt(1000.4,801.877,335.128);
-      float charge = 100000;
-	
-	
-	// decide which apa the charge deposit belongs to
-	short which_cryo = det_gds.in_which_cryo(pt);
-	short which_apa = det_gds.in_which_apa(pt);
-	// std::cout<<"point ("<<pt.x<<", "<<pt.y<<", "<<pt.z<<") is in the "<<which_cryo<<"th "
-	// 	 <<"cryo and the "<<which_apa<<"th apa"<<std::endl;
-	apa_gds = det_gds.get_apaGDS(det_gds.in_which_cryo(pt), det_gds.in_which_apa(pt));
+      const Point& pt = hits[ind].first;
+      //const Vector vpt(pt.x, pt.y, pt.z);
+      float charge = hits[ind].second;
+      
+      //const Point pt(1000.4,-301.877,135.128);
+      //float charge = 100000;
+      
+      
+      // decide which apa the charge deposit belongs to
+      short which_cryo = det_gds.in_which_cryo(pt);
+      short which_apa = det_gds.in_which_apa(pt);
+      // std::cout<<"point ("<<pt.x<<", "<<pt.y<<", "<<pt.z<<") is in the "<<which_cryo<<"th "
+      // 	 <<"cryo and the "<<which_apa<<"th apa"<<std::endl;
+      apa_gds = det_gds.get_apaGDS(det_gds.in_which_cryo(pt), det_gds.in_which_apa(pt));
 	if (apa_gds==NULL) {
 	  // out side the detector boundary ... 
 	  //  std::cout<<"exit because apa_gds = NULL"<<std::endl; 
